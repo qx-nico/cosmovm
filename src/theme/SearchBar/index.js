c0e7e1547e9d9c0cbb0fd0c687bdac9e80044c36
Nico Poggi <poggi.nicco@gmail.com>
2025-03-11 01:28:27 +0000

    Update index.js

--- src/theme/SearchBar/index.js
+++ src/theme/SearchBar/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import SearchBar from '@theme-original/SearchBar';
-import AskCookbook from '@cookbookdev/docsbot/react'
 import BrowserOnly from '@docusaurus/BrowserOnly';
 
 /** It's a public API key, so it's safe to expose it here */
-const COOKBOOK_PUBLIC_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NmIyNTY3ZDM3ZWIwYzRiMTVlYWE1YjIiLCJpYXQiOjE3MjI5NjM1ODEsImV4cCI6MjAzODUzOTU4MX0.vpVV5fCo1WcnruoFu1XKxYWY5ewzSfYvzLkoJYjojD4";
+const COOKBOOK_PUBLIC_API_KEY = "";
 
 export default function SearchBarWrapper(props) {
   return (
