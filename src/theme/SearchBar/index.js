a5498ae6914fe0a7766290c14456bc5d5685147a
Nico Poggi <poggi.nicco@gmail.com>
2025-03-11 01:33:58 +0000

    Update index.js

--- src/theme/SearchBar/index.js
+++ src/theme/SearchBar/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import SearchBar from '@theme-original/SearchBar';
+import AskCookbook from '@cookbookdev/docsbot/react'
 import BrowserOnly from '@docusaurus/BrowserOnly';
 
 /** It's a public API key, so it's safe to expose it here */
